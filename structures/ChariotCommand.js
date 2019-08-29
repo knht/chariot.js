2c714a74ab9778423e4b8fe428cf14431ed8be9b
Riya <kato.hana@outlook.jp>
2019-08-29 12:05:15 +0200

    Added default command executor

--- structures/ChariotCommand.js
+++ structures/ChariotCommand.js
@@ -1,6 +1,10 @@
 /**
  * Basic abstract Command class for command identification
  */
-class ChartiotCommand {}
+class ChartiotCommand {
+    execute() {
+        throw new Error('No command logic implemented! Please check the official example on GitHub. https://github.com/riyacchi/chariot.js-example');
+    }
+}
 
 module.exports = ChartiotCommand;
