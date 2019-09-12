570c2b1f17521b4925a04d8f7fc71d49913826cf
Riya <kato.hana@outlook.jp>
2019-09-12 11:20:02 +0200

    Added v4 UUID generator

--- helpers/Util.js
+++ helpers/Util.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 class Util {
     /**
      * Formats a permission name
@@ -8,6 +10,19 @@ class Util {
         const result = permission.split(/(?=[A-Z])/).join(' ');
         
         return result.charAt(0).toUpperCase() + result.slice(1);
+    }
+
+    /**
+     * Generate RFC4122 compliant v4 UUIDs
+     * @returns {string} A v4 UUID
+     */
+    uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(
+            /[018]/g, 
+            (char) => { 
+                return (char ^ crypto.randomFillSync(new Uint8Array(1))[0] & 15 >> char / 4).toString(16) 
+            }
+        );
     }
 }
 
